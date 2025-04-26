7e9c82b006eba0e4a7e9e70b45fc03d6cbae1feb
thewebsitecreator244 <alexkudinov1300@gmail.com>
2025-04-26 10:48:36 +0200

    feat added slider "

---
+++ js/slider.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $('#slider').slick({
+        dots: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1,
+    });                                 
+});
