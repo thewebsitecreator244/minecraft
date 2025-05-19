6ebbaa11883d39f88f3e05f511a18c69f1639c27
thewebsitecreator244 <alexkudinov1300@gmail.com>
2025-05-19 18:18:03 +0200

    feat addedd *****mer img and sound

--- js/screamer.js
+++ js/screamer.js
@@ -1,7 +1,12 @@
 const btnScreamer = document.querySelector("#btnScreamer");
 
 function screamer(){
-    console.log('bwahahahahahahah');
+    const screamerImg = new Image()
+    screamerImg.src = "../img/zumb.jpeed.jpg"
+    screamerImg.className = "screamer-img"
+    document.body.append(screamerImg)
+    const screamerAudio = new Audio("../sound/evil-laugh-sound.mp3");
+    screamerAudio.play()
 };
 
 btnScreamer.addEventListener("click", screamer);
